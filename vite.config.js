5f38864405a28168cf71cafd8871fcc2dfa06f87
nyer <ls5231@gmail.com>
2025-02-27 09:20:22 +0800

    update css module config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase', // 类名转换为驼峰命名
+      scopeBehaviour: 'local', // 启用局部作用域
+      generateScopedName: '[name]__[local]___[hash:base64:5]', // 自定义类名格式
+    },
+  },
   base: './', // 设置基础路径为当前目录
 });
